08bee6d193b39abf90eb90e9e6c72595bb1e960e
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-04-18 12:04:02 +0800

    add teh forms for login and sign_up pages

--- app.rb
+++ app.rb
@@ -11,4 +11,21 @@ get("/") do
   erb(:index)
 end
 
+get("/login") do
+  erb(:login)
+end
+
+get("/sign_up") do
+  erb(:sign_up)
+end
+
+post("/sign_up") do
+  name = params.fetch('name')
+  username = params.fetch('username')
+  image = params.fetch('image')
+  password = params.fetch('password')
+  user = User.create({:username => username, :name => name, :image => image, :password =>password}) #create is the equivalent of user = User.new plus user.save()
+  redirect('/user')
+end
+
 # test kevin
