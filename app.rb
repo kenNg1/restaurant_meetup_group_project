002b1994da27e4c59f8d4a02f04b0c1f8a0d7df0
Jeffrey Li <jeffrey.li@cantab.net>
2017-04-19 09:53:53 +0800

    changed app.rb on redirect

--- app.rb
+++ app.rb
@@ -50,7 +50,7 @@ post('/users') do
   # not yet complete
   if @user != nil
     session[:id] = @user.id
-    redirect('/success')
+    redirect('/user')
   else
     redirect('/login')
   end
@@ -68,7 +68,7 @@ post("/sign_up") do
   @user = User.new({:username => username, :name => name, :image => image, :password =>password})
   if @user.save()
     session[:id] = @user.id
-    redirect('/success')
+    redirect('/user')
   else
     erb(:errors)
   end
@@ -92,7 +92,7 @@ post("/admin") do
   redirect('/admin')
 end
 
-get("/user/:id") do
+get("/user") do
   @cuisines = Cuisine.all()
   @districts = District.all()
   @budgets = Budget.all()
