398afe981d73a28bb637d29ed63256714a57ab51
Kevin <cho.kevin@hotmail.com>
2017-04-19 14:48:38 +0800

    merge conflicts solving

--- app.rb
+++ app.rb
@@ -50,7 +50,7 @@ post('/users') do
   # not yet complete
   if @user != nil
     session[:id] = @user.id
-    redirect('/success')
+    redirect('/user')
   else
     redirect('/login')
   end
@@ -68,7 +68,7 @@ post("/sign_up") do
   @user = User.new({:username => username, :name => name, :image => image, :password =>password})
   if @user.save()
     session[:id] = @user.id
-    redirect('/success')
+    redirect('/user')
   else
     erb(:errors)
   end
@@ -76,13 +76,6 @@ end
 #test
 get "/success", :auth => :user do
   erb(:success)
-end
-
-get("/user") do
-  erb(:user)
-  password = params.fetch('password')
-  User.create({:username => username, :name => name, :image => image, :password =>password}) #create is the equivalent of user = User.new plus user.save()
-  redirect('/')
 end
 
 get("/admin") do
